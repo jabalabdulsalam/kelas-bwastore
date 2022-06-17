a3feedb17f951554672326df3c46b785eee31440
ceikdo <jabalabdul71@gmail.com>
2022-06-17 16:36:59 +0700

    Inisialisasi kode kelas-bwastore

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        error_reporting(E_ALL & E_NOTICE & E_STRICT & E_DEPRECATED & E_USER_DEPRECATED);
+        Paginator::useBootstrap();
+    }
+}
